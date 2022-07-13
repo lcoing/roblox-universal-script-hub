2e69209de74aaf0f3f48f18696c1ca42f253e8d6
lcoing <109206641+lcoing@users.noreply.github.com>
2022-07-13 16:41:57 +0700

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("roblox universal client exploit", "Synapse")
+local Window = Library.CreateLib("roblox universal client exploit by idkman", "Synapse")
 
 --MAIN
 local Main = Window:NewTab("main script")
